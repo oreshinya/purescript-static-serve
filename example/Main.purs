5ff6d745fc0f7a12fcfb9b60dc10af047771003d
Shinya Takahashi <s.takahashi313@gmail.com>
2018-01-30 02:05:23 +0900

    Manage conditional get

--- example/Main.purs
+++ example/Main.purs
@@ -3,6 +3,7 @@ module Main where
 import Prelude
 
 import Control.Monad.Eff (Eff)
+import Data.JSDate (LOCALE)
 import Data.Maybe (Maybe(..))
 import Node.FS (FS)
 import Node.HTTP (HTTP, ListenOptions, createServer, listen)
@@ -19,7 +20,7 @@ config =
 
 
 
-main :: forall e. Eff (fs :: FS, http :: HTTP | e) Unit
+main :: forall e. Eff (fs :: FS, http :: HTTP, locale :: LOCALE | e) Unit
 main = do
   server <- createServer $ staticHandler { root: "./public", maxAge: 60 }
   listen server config $ pure unit
--- src/StaticServe.purs
+++ src/StaticServe.purs
@@ -10,17 +10,21 @@ import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Exception (Error)
 import Data.Either (Either(..))
 import Data.Foldable (elem)
-import Data.JSDate (toUTCString)
-import Data.Maybe (fromMaybe)
+import Data.JSDate (JSDate, LOCALE, getTime, parse, toUTCString)
+import Data.Maybe (Maybe(..), fromMaybe, maybe)
 import Data.Nullable (toMaybe)
+import Data.StrMap (lookup)
+import Data.String.Regex (test)
+import Data.String.Regex.Flags (noFlags)
+import Data.String.Regex.Unsafe (unsafeRegex)
 import Node.FS (FS)
 import Node.FS.Async (stat)
 import Node.FS.Stats (Stats(..))
 import Node.FS.Stream (createReadStream)
-import Node.HTTP (HTTP, Request, Response, requestMethod, requestURL, responseAsStream, setHeader, setStatusCode)
+import Node.HTTP (HTTP, Request, Response, requestHeaders, requestMethod, requestURL, responseAsStream, setHeader, setStatusCode)
 import Node.Path (resolve)
 import Node.Stream (end, onError, pipe)
-import Node.URL (parse)
+import Node.URL as URL
 import StaticServe.ContentType (contentTypeFromPath)
 
 
@@ -47,13 +51,33 @@ isHead req = requestMethod req == "HEAD"
 
 
 
-staticHandler :: forall e. Settings -> (Request -> Response -> Eff (http :: HTTP, fs :: FS | e) Unit)
+requestHeader :: String -> Request -> Maybe String
+requestHeader key req = lookup key $ requestHeaders req
+
+
+
+fresh :: forall e. Request -> JSDate -> Eff (locale :: LOCALE | e) Boolean
+fresh req lastModified =
+  case requestHeader "if-modified-since" req of
+    Nothing -> pure false
+    Just timeStr -> do
+      ifModifiedSince <- parse timeStr
+      if isNoCache
+        then pure false
+        else pure $ getTime lastModified <= getTime ifModifiedSince
+  where
+    isNoCache = maybe false includeNoCache $ requestHeader "cache-control" req
+    includeNoCache str = flip test str $ unsafeRegex "(?:^|,)\\s*?no-cache\\s*?(?:,|$)" noFlags
+
+
+
+staticHandler :: forall e. Settings -> (Request -> Response -> Eff (http :: HTTP, fs :: FS, locale :: LOCALE | e) Unit)
 staticHandler { root, maxAge } req res =
   if isHeadOrGet req
     then stat fullPath handleStat
     else handleInvalidMethod
   where
-    path = fromMaybe "/" $ toMaybe $ _.pathname $ parse $ requestURL req
+    path = fromMaybe "/" $ toMaybe $ _.pathname $ URL.parse $ requestURL req
 
     fullPath = resolve [ root ] $ "." <>
       if path == "/"
@@ -77,11 +101,22 @@ staticHandler { root, maxAge } req res =
       setHeader res "Content-Type" $ contentTypeFromPath fullPath
       setHeader res "Last-Modified" $ toUTCString stats.mtime
       setHeader res "Cache-Control" $ "max-age=" <> show maxAge
-      setStatusCode res 200
       if isHead req
-        then
+        then handleHead
+        else handleGet stats
+
+    handleHead = do
+      setStatusCode res 200
+      end (responseAsStream res) $ pure unit
+
+    handleGet stats = do
+      isFresh <- parse (toUTCString stats.mtime) >>= fresh req
+      if isFresh
+        then do
+          setStatusCode res 304
           end (responseAsStream res) $ pure unit
         else do
+          setStatusCode res 200
           readable <- createReadStream fullPath
           onError readable \err -> handleStat $ Left err
           void $ pipe readable $ responseAsStream res
